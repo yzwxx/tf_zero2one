24fc21357f882b6789de976f59a122527635dee9
zy5015 <zy5015@ic.ac.uk>
2017-05-16 20:07:00 +0100

    update

--- deepMnist.py
+++ deepMnist.py
@@ -79,26 +79,26 @@ def deepnn(x):
   # First convolutional layer - maps one grayscale image to 32 feature maps.
   W_conv1 = weight_variable([5, 5, 1, 32])
   b_conv1 = bias_variable([32])
-  h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+  h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # [batch_size,28,28,32]
 
   # Pooling layer - downsamples by 2X.
-  h_pool1 = max_pool_2x2(h_conv1)
+  h_pool1 = max_pool_2x2(h_conv1) # [batch_size,14,14,32]
 
   # Second convolutional layer -- maps 32 feature maps to 64.
   W_conv2 = weight_variable([5, 5, 32, 64])
   b_conv2 = bias_variable([64])
-  h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+  h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) # [batch_size,14,14,64]
 
   # Second pooling layer.
-  h_pool2 = max_pool_2x2(h_conv2)
+  h_pool2 = max_pool_2x2(h_conv2) # [batch_size,7,7,64]
 
   # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
   # is down to 7x7x64 feature maps -- maps this to 1024 features.
   W_fc1 = weight_variable([7 * 7 * 64, 1024])
   b_fc1 = bias_variable([1024])
 
-  h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-  h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+  h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64]) # [batch_size,7*7*64]
+  h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1) # [batch_size,1024]
 
   # Dropout - controls the complexity of the model, prevents co-adaptation of
   # features.
@@ -109,7 +109,7 @@ def deepnn(x):
   W_fc2 = weight_variable([1024, 10])
   b_fc2 = bias_variable([10])
 
-  y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+  y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2 # [batch_size,10]
   return y_conv, keep_prob
 
 
@@ -133,7 +133,7 @@ def main(_):
   train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
   # accuracy for testing
-  correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+  correct_prediction = tf.equal(tf.argmax(y_conv, axis=1), tf.argmax(y_, axis=1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
   with tf.Session() as sess:
